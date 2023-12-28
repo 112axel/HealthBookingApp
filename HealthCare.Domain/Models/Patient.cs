ee8c5b0a3344caa4fc575752964c0c40db85a031
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-12-28 03:00:58 +0100

    models added

---
+++ HealthCare.Domain/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Domain.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+}
---
+++ HealthCare.Domain/Models/Staff.cs
@@ -0,0 +1,19 @@
+﻿using HealthCare.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Domain.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public Role Role { get; set; }
+        public List<Appointment> Appointments { get; set; }
+        public Schedule Schedule { get; set; }
+        
+    }
+}
