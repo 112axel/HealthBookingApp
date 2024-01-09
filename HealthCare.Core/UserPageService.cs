fd56015f7c01e2c0832d1f7c2443c79800db7aa8
Axel Cullbrand <axel.cullbrand@gmail.com>
2024-01-09 13:59:53 +0100

    fixed some errors

--- HealthCare.Core/UserPageService.cs
+++ HealthCare.Core/UserPageService.cs
@@ -16,7 +16,7 @@ namespace HealthCare.Core
                 new Patient
             {
                 Id = 1,
-                Account = new Account { Id = 1, FirstName = "John", LastName = "Doe", Email = "john@example.com", Password = "hashedPassword", Salt = "salt" },
+                Account = new Account {Id = "2", FirstName = "John", LastName = "Doe", Email = "john@example.com"},
                 Appointments = new List<Appointment>
                 {
                     new Appointment { Id = 1, DateTime = DateTime.Now, Service = "Checkup" },
@@ -25,7 +25,7 @@ namespace HealthCare.Core
             new Patient
             {
                 Id = 2,
-                Account = new Account { Id = 2, FirstName = "Jane", LastName = "Doe", Email = "jane@example.com", Password = "hashedPassword", Salt = "salt" },
+                Account = new Account { Id = "2", FirstName = "Jane", LastName = "Doe", Email = "jane@example.com"},
                 Appointments = new List<Appointment>
                 {
                     new Appointment { Id = 2, DateTime = DateTime.Now.AddDays(7), Service = "Cleaning" },
