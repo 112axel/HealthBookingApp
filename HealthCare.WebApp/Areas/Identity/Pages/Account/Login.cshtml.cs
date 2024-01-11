362009a9266d60cbed0553e574f4095c99c2ee81
Axel Cullbrand <axel.cullbrand@gmail.com>
2024-01-11 11:04:19 +0100

    moved database to its own class library

--- HealthCare.WebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HealthCare.WebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -120,7 +120,7 @@ namespace HealthCare.WebApp.Areas.Identity.Pages.AccountNamespace
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, Input.RememberMe });
                 }
                 if (result.IsLockedOut)
                 {
