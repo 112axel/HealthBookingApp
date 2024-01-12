a3d6f2cb36f21ea4d38da21c675aa96d7dd0a111
Vellethe <95373802+Vellethe@users.noreply.github.com>
2024-01-12 10:21:31 +0100

    Doctor page outcommented

--- HealthCare.Core/DoctorPageService.cs
+++ HealthCare.Core/DoctorPageService.cs
@@ -18,13 +18,13 @@ namespace HealthCare.Core
             this.healthContext = healthContext;
         }
 
-        public Doctor GetDoctorById(string accountId)
-        {
-            // Retrieve the doctor by ID from the database
-            return healthContext.Doctors
-                .Include(p => p.Account)
-                .Include(p => p.Schedule)
-                .FirstOrDefault(p => p.Account.Id == accountId);
-        }
+        //public Doctor GetDoctorById(string accountId)
+        //{
+        //    // Retrieve the doctor by ID from the database
+        //    return healthContext.Doctors
+        //        .Include(p => p.Account)
+        //        .Include(p => p.Schedule)
+        //        .FirstOrDefault(p => p.Account.Id == accountId);
+        //}
     }
 }
