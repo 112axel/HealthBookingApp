814f22afc41bf9f386a289e3d169e77977da11f8
Axel Cullbrand <axel.cullbrand@gmail.com>
2024-01-22 15:07:10 +0100

    shows name instead of email

--- HealthCare.WebApp/Program.cs
+++ HealthCare.WebApp/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<AppointmentService>();
 builder.Services.AddScoped<BookingService>();
 builder.Services.AddScoped<UserPageService>();
 builder.Services.AddScoped<DoctorPageService>();
+builder.Services.AddScoped<AccountService>();
 
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<Account>>();
 
