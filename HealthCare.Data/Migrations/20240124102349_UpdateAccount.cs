7fcdb1ae75c6d701613a38a9b07ed6bc805156f9
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2024-01-24 16:12:24 +0100

    Done with userPage
    fixed nav a little bit and added age to Account with migration.

---
+++ HealthCare.Data/Migrations/20240124102349_UpdateAccount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthCare.WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+        }
+    }
+}
