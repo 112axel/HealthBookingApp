207553ae79d7165d212718ba6d1f1114e94a90b2
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2024-01-26 11:25:10 +0100

    Fixed changes

--- HealthCare.Core/UserPageService.cs
+++ HealthCare.Core/UserPageService.cs
@@ -18,34 +18,19 @@ namespace HealthCare.Core
             this.healthContext = healthContext;
         }
 
-        public void UpdateUser(Staff? currentDoctor, Patient? currentPatient, string firstName, string lastName, int? age, string email, string phoneNumber)
+        public void UpdateUser(Account currentUser, string firstName, string lastName, int? age, string email, string phoneNumber)
         {
-            if (currentDoctor != null)
+            if (currentUser != null)
             {
-                // Update properties of the existing patient with the new values
-                currentDoctor.Account.FirstName = firstName;
-                currentDoctor.Account.LastName = lastName;
-                currentDoctor.Account.Age = age;
-                currentDoctor.Account.Email = email;
-                currentDoctor.Account.PhoneNumber = phoneNumber;
+                // Update properties of the existing user with the new values
+                currentUser.FirstName = firstName;
+                currentUser.LastName = lastName;
+                currentUser.Age = age;
+                currentUser.Email = email;
+                currentUser.PhoneNumber = phoneNumber;
 
                 // Mark the entity as modified
-                healthContext.Entry(currentDoctor).State = EntityState.Modified;
-
-                // Save changes to the database
-                healthContext.SaveChanges();
-            }
-            else if (currentPatient != null)
-            {
-                // Update properties of the existing patient with the new values
-                currentPatient.Account.FirstName = firstName;
-                currentPatient.Account.LastName = lastName;
-                currentPatient.Account.Age = age;
-                currentPatient.Account.Email = email;
-                currentPatient.Account.PhoneNumber = phoneNumber;
-
-                // Mark the entity as modified
-                healthContext.Entry(currentPatient).State = EntityState.Modified;
+                healthContext.Entry(currentUser).State = EntityState.Modified;
 
                 // Save changes to the database
                 healthContext.SaveChanges();
